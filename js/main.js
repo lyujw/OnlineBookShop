ad51ee8ea3b8c052e6e0777531010d423347b684
Stan-Lyu-0309 <1962842919@qq.com>
2020-07-04 22:31:34 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function( $ ){
+  $('.bxslider').bxSlider();
+
+  $(".tab_item").not(":first").hide();
+$(".wrapper .tab").click(function() {
+	$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+ });
